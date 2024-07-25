907ecbd2a599b069e6b151989867292b0b71964c
AlpSari <50492914+AlpSari@users.noreply.github.com>
2024-07-25 21:41:19 +0200

    entities: use movement logic to update position of the character

--- omg/entities/player.py
+++ omg/entities/player.py
@@ -1,20 +1,28 @@
 import arcade
 import math
+from mechanics import movement
 from entities.projectile import Projectile
 from mechanics.collision import handle_obstacle_collisions, handle_screen_boundary_collision
 
+MOVEMENT_SPEED_FORWARD = 1
+MOVEMENT_SPEED_SIDE = 1
 class Player(arcade.Sprite):
-    def __init__(self, name, char_class, image_file, scale, initial_angle=0):
+    def __init__(self, name, char_class, image_file, scale, screen_width, screen_height, initial_angle=0):
         super().__init__(image_file, scale)
         self.name = name
         self.char_class = char_class
         self.center_x = 100
         self.center_y = 100
-        self.change_x = 0
-        self.change_y = 0
         self.projectiles = arcade.SpriteList()
+        self.screen_width = screen_width
+        self.screen_height = screen_height
         self.angle = initial_angle  # Set initial angle here
 
+        # Movement
+        self.movement_logic = movement.CompassDirected(arcade.key.W, arcade.key.S, arcade.key.A, arcade.key.D)
+        # self.movement_logic = movement.MouseDirected(arcade.key.W, arcade.key.S, arcade.key.A, arcade.key.D)
+        self.change_x = MOVEMENT_SPEED_SIDE
+        self.change_y = MOVEMENT_SPEED_FORWARD
         # Health
         self.max_health = 100
         self.current_health = 100
@@ -25,22 +33,26 @@ class Player(arcade.Sprite):
         self.mana_regen_rate = 5  # Mana regenerated per second
         self.mana_regen_cooldown = 0
 
-        # Default movement keys
-        self.key_forward = arcade.key.W
-        self.key_backward = arcade.key.S
-        self.key_left = arcade.key.A
-        self.key_right = arcade.key.D
-
         # Projectile types
         self.projectile_types = []
         self.current_projectile_index = 0
 
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        self.movement_logic.on_key_press(key, modifiers)
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+        self.movement_logic.on_key_release(key, modifiers)
+
     def update(self, mouse_x, mouse_y, obstacles, screen_width, screen_height, delta_time):
-        self.update_position()
+
+        (self.center_x, self.center_y, self.angle) = self.movement_logic.calculate_player_state(
+            self.center_x, self.center_y, mouse_x, mouse_y, self.change_x, self.change_y
+        )
         handle_obstacle_collisions(self, obstacles)
         handle_screen_boundary_collision(self, screen_width, screen_height)
         self.projectiles.update()
-        self.face_mouse(mouse_x, mouse_y)
         self.regenerate_mana(delta_time)
 
     def update_position(self):
@@ -61,7 +73,7 @@ class Player(arcade.Sprite):
     def shoot(self):
         if not self.projectile_types or self.current_mana < 20:
             return
-        
+
         self.current_mana -= 20
         projectile_info = self.projectile_types[self.current_projectile_index]
         projectile = Projectile(
@@ -100,10 +112,10 @@ class Player(arcade.Sprite):
             self.current_projectile_index = index
 
     def set_movement_keys(self, forward, backward, left, right):
-        self.key_forward = forward
-        self.key_backward = backward
-        self.key_left = left
-        self.key_right = right
+        self.movement_logic.key_forward = forward
+        self.movement_logic.key_backward = backward
+        self.movement_logic.key_left = left
+        self.movement_logic.key_right = right
 
     def draw(self):
         super().draw()
