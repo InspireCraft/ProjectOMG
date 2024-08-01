6f2be94e444122f840e53d40e01e4af29ba4cec1
AlpSari <50492914+AlpSari@users.noreply.github.com>
2024-08-01 18:48:16 +0200

    mechanics: fix linting - collision

--- omg/mechanics/collision.py
+++ omg/mechanics/collision.py
@@ -7,6 +7,7 @@ from omg.entities.projectile import Projectile
 def handle_projectile_collisions(
     projectiles: List[Projectile], obstacles: List[Obstacle]
 ):
+    """Projectile - obstacle collison logic."""
     for projectile in projectiles:
         hit_list: List[Obstacle] = arcade.check_for_collision_with_list(
             projectile, obstacles
