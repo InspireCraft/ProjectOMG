bc89c069de5ae7b81d3c5b50c0cc556a599a96c3
AlpSari <50492914+AlpSari@users.noreply.github.com>
2024-08-11 12:26:19 +0200

    events: change pickup logic to publish a sprite to indicate a hitbox

--- omg/entities/events.py
+++ omg/entities/events.py
@@ -1,3 +1,5 @@
+import arcade
+from omg.entities.items import ItemManager
 from omg.structural.observer import Event
 from omg.entities.projectile import Projectile
 
@@ -5,11 +7,12 @@ from omg.entities.projectile import Projectile
 class PickupRequestEvent(Event):
     """Event triggered when an entity wants to pick up an item from the ground."""
 
-    def __init__(self, entity, entity_pos_x, entity_pos_y, entity_pickup_radius):
+    def __init__(
+        self, entity, entity_pickup_sprite
+    ):
         super().__init__("pickup_request")
-        self.entity = entity
-        self.entity_pos = (entity_pos_x, entity_pos_y)
-        self.pickup_radius = entity_pickup_radius
+        self.entity: ItemManager = entity
+        self.entity_pickup_sprite: arcade.Sprite = entity_pickup_sprite
 
 
 class ProjectileShotEvent(Event):
