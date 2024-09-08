076feebd2ed87bb4d2e6d563425f4d176498746b
AlpSari <50492914+AlpSari@users.noreply.github.com>
2024-09-08 19:00:00 +0200

    core: put views into different module, clear keys and references in GameWindow

--- omg/core/views.py
+++ omg/core/views.py
@@ -1,73 +1,30 @@
+import os.path
 from typing import Dict
 
 import arcade
-import arcade.key
 
+from omg.entities.events import PickupRequestEvent, ProjectileShotEvent
 from omg.entities.items import Pickupable
 from omg.entities.player import Player
+from omg.entities.projectile import (
+    FireballFactory,
+    IceShardFactory,
+)
 from omg.entities.obstacle import Obstacle
-
 from omg.mechanics.collision import handle_projectile_collisions
 from omg.mechanics.physics import PhysicsEngineBoundary
 from omg.structural.observer import Observer
-from omg.entities.events import PickupRequestEvent, ProjectileShotEvent
-from omg.entities.elements import ELEMENTS
 
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "2D Shooter RPG"
 
 ASSET_DIR = os.path.join(
     os.path.join(os.path.dirname(__file__), ".."), "assets", "images"
 )
 
 COIN_IMAGE_PATH = ":resources:images/items/coinGold.png"
-from omg.core.views import (
-    GameView,
-    PauseView,
-    SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
-)
 
-
-class GameWindow(arcade.Window):
-    """Main game window."""
-
-    GAME_VIEW_KEY = "game"
-    PAUSE_VIEW_KEY = "pause"
-
-    def __init__(self, width: int, height: int, title: str):
-        super().__init__(width, height, title)
-        self.views: Dict[str, arcade.View] = None
-
-    def setup(self):
-        """Setup the game."""
-        self.views: Dict[str, arcade.View] = {}
-        self.views[self.GAME_VIEW_KEY] = GameView(window=self)
-        self.views[self.PAUSE_VIEW_KEY] = PauseView(window=self)
-        self.binary_state = True  # TODO: Refactor later
-
-        self._game_view.setup()
-        self.show_view(self.views[self.GAME_VIEW_KEY])
-
-    @property
-    def _game_view(self) -> GameView:
-        """Define self._game_view which always refers to a View in self.views."""
-        return self.views.get(self.GAME_VIEW_KEY, None)
-
-    @property
-    def _pause_view(self) -> PauseView:
-        """Define self._pause_view which always refers to a View in self.views."""
-        return self.views.get(self.PAUSE_VIEW_KEY, None)
-
-    def on_key_press(self, key, modifiers):
-        """Key press logic."""
-        if key == arcade.key.ESCAPE:
-            self.binary_state = not self.binary_state
-            if self.binary_state:
-                self.show_view(self._game_view)
-            else:
-                self.show_view(self.views["pause"])
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "2D Shooter RPG"
 
 
 class GameView(arcade.View):
@@ -81,15 +38,12 @@ class GameView(arcade.View):
         self.pickupables: arcade.SpriteList = (
             None  # items that can be pickedup from the ground
         )
-        self.skill_slot_1: arcade.Sprite = None  # Skill slot 1
-        self.skill_slot_2: arcade.Sprite = None  # SKill slot 2
         self.projectiles = None
         self.physics_engine = None
         self.mouse_x = 0
         self.mouse_y = 0
         self.icon_scale = 0.1
-        self.icon_margin_x = 10
-        self.icon_margin_y = 75
+        self.icon_margin = 10
         self.icon_size = 64
         self.active_keys: Dict[tuple, bool] = None
 
@@ -120,22 +74,6 @@ class GameView(arcade.View):
         obstacle.center_y = 300
         self.obstacles.append(obstacle)
 
-        # Create crafted skill slots
-        # Skill slot 1
-        scale_factor_1 = 0.4
-        skill_slot_1_img = os.path.join(ASSET_DIR, "skill_slots_d_f", "D.png")
-        self.skill_slot_1 = arcade.Sprite(skill_slot_1_img, scale=scale_factor_1)
-        self.skill_slot_1.center_x = self.skill_slot_1.width // 2
-        self.skill_slot_1.center_y = self.skill_slot_1.height // 2
-
-        # Skill slot 2
-        scale_factor_2 = 0.4
-        skill_slot_2_img = os.path.join(ASSET_DIR, "skill_slots_d_f", "F.png")
-        self.skill_slot_2 = arcade.Sprite(skill_slot_2_img, scale=scale_factor_2)
-        self.skill_slot_2.center_x = self.skill_slot_1.center_x + \
-            self.skill_slot_2.width
-        self.skill_slot_2.center_y = self.skill_slot_2.height // 2
-
         self.physics_engine = PhysicsEngineBoundary(
             player_sprite=self.player,
             walls=self.obstacles,
@@ -144,32 +82,32 @@ class GameView(arcade.View):
         )
         # Add projectile types
         self.pickupables.append(
-            Pickupable(COIN_IMAGE_PATH, 0.5, ELEMENTS["FIRE"], 150, 10)
+            Pickupable(COIN_IMAGE_PATH, 0.5, FireballFactory, 150, 10)
         )
         self.pickupables.append(
-            Pickupable(COIN_IMAGE_PATH, 0.5, ELEMENTS["ICE"], 250, 20)
+            Pickupable(COIN_IMAGE_PATH, 0.5, IceShardFactory, 250, 20)
         )
         self.pickupables.append(
-            Pickupable(COIN_IMAGE_PATH, 0.5, ELEMENTS["FIRE"], 250, 120)
+            Pickupable(COIN_IMAGE_PATH, 0.5, FireballFactory, 250, 120)
         )
 
     @property
-    def element_icons(self):
-        """Define self.element_icons."""
-        # TODO: refactor element_icons logic if causes a performance bottleneck
-        # For example "element_acquired event" can trigger an update when necessary
+    def skill_icons(self):
+        """Define self.skill_icons."""
+        # TODO: refactor skill_icons logic if causes a performance bottleneck
+        # For example "skill_acquired event" can trigger an update when necessary
         if self.player:
             return [
-                arcade.load_texture(element_type["image_file"])
-                for element_type in self.player.elements
+                arcade.load_texture(projectile_type.image_file)
+                for projectile_type in self.player.skills
             ]
         else:
             return None
 
-    def _load_element_icons(self):
-        for element_type in self.player.elements:
-            icon_texture = arcade.load_texture(element_type["image_file"])
-            self.element_icons.append(icon_texture)
+    def _load_skill_icons(self):
+        for projectile_type in self.player.skills:
+            icon_texture = arcade.load_texture(projectile_type.image_file)
+            self.skill_icons.append(icon_texture)
 
     def on_draw(self):
         """Drawing code."""
@@ -250,11 +188,10 @@ class GameView(arcade.View):
         self.active_keys = {}  # reset the dictionary
 
     def _draw_ui(self):
-        # Draw picked up elements to top left corner
-        for i, icon in enumerate(self.element_icons):
-            x = self.icon_margin_x + i * (self.icon_size + self.icon_margin_x)
-            y = SCREEN_HEIGHT - self.icon_margin_y
-            if i == self.player.elements.get_current_index():
+        for i, icon in enumerate(self.skill_icons):
+            x = self.icon_margin + i * (self.icon_size + self.icon_margin)
+            y = self.icon_margin
+            if i == self.player.skills.get_current_index():
                 arcade.draw_rectangle_outline(
                     x + self.icon_size // 2,
                     y + self.icon_size // 2,
@@ -271,50 +208,25 @@ class GameView(arcade.View):
                 icon,
             )
 
-        # Draw combined skill icons
-        if self.player.crafted_skill_slots[0] is not None:
-            scale_factor_crafted_skill_1 = 0.3
-            skill_1_img = os.path.join(
-                ASSET_DIR,
-                "skills",
-                f"{self.player.crafted_skill_slots[0]}.png"
-            )
-            skill_1 = arcade.Sprite(skill_1_img, scale=scale_factor_crafted_skill_1)
-            skill_1.center_x = skill_1.width // 2
-            skill_1.center_y = skill_1.height // 2
-            skill_1.draw()
-
-        if self.player.crafted_skill_slots[1] is not None:
-            scale_factor_crafted_skill_2 = 0.3
-            skill_2_img = os.path.join(
-                ASSET_DIR,
-                "skills",
-                f"{self.player.crafted_skill_slots[1]}.png"
-            )
-            skill_2 = arcade.Sprite(skill_2_img, scale=scale_factor_crafted_skill_2)
-            skill_2.center_x = skill_2.width // 2 + skill_2.width
-            skill_2.center_y = skill_2.height // 2
-            skill_2.draw()
-
-        # Draw usable skill slots to bottom left corner
-        self.skill_slot_1.draw()
-        self.skill_slot_2.draw()
-
 
 class PauseView(arcade.View):
     """Pause screen of the game."""
 
-    def __init__(self, window: GameWindow):
+    def __init__(self, window: arcade.Window):
         super().__init__(window)
-        self.game_window = window  # Add additional reference for type-hinting
+        self._view_to_draw: arcade.View = None
+
+    def update_view_to_draw(self, new_view):
+        """Update the View to be drawn in the pause state."""
+        self._view_to_draw = new_view
 
     def on_draw(self):
         """Draw the game as is and the pause view extras."""
         self.clear()
-        game_view = self.game_window.views.get("game", None)
-        if game_view:
-            game_view.on_draw()
+        if self._view_to_draw:
+            self._view_to_draw.on_draw()
 
+        # Draw the Pause text on the given View as overlay
         arcade.draw_text("PAUSED", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 50,
                          arcade.color.WHITE, font_size=50, anchor_x="center")
 
@@ -336,19 +248,3 @@ class PauseView(arcade.View):
         """Key release logic."""
         if key == arcade.key.Q:
             arcade.exit()
-                # Draw last game state when paused
-                self._pause_view.update_view_to_draw(self._game_view)
-                self.show_view(self._pause_view)
-
-
-def main():
-    """Main application code."""
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    start_view = GameView()
-    window.show_view(start_view)
-    start_view.setup()
-    arcade.run()
-
-
-if __name__ == "__main__":
-    main()
