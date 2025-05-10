7f3dce1bab90691c827872ce51711a34a95e386b
Arda Aralıoğlu <arda639@gmail.com>
2025-05-10 17:49:45 +0200

    Updated animations.py on_key_press function for better scalability

--- omg/mechanics/animations.py
+++ omg/mechanics/animations.py
@@ -32,12 +32,14 @@ class Animations():
 
     def __init__(self, slash_key, cast_key, thrust_key, shoot_key, move_logic):
 
-        self.slash_key = slash_key
-        self.cast_key = cast_key
-        self.thrust_key = thrust_key
-        self.shoot_key = shoot_key
+        self.key_to_action_dict = {
+            slash_key: SLASH,
+            cast_key: SPELLCAST,
+            thrust_key: THRUST,
+            shoot_key: SHOOT,
+         }
 
-        self.player_state = IDLE
+        self._player_state = IDLE
         self.character_face_direction = DOWN_FACING
         self.player_texture = ""
 
@@ -64,41 +66,36 @@ class Animations():
         # Action finish checker
         self.action_finished = 0
 
-        self.player_change_x = 0
-        self.player_change_y = 0
-
     @property
     def move_direction(self):
         return self.move_logic.move_direction
 
     def update(self, delta_time, player_change_x, player_change_y):
         # Movement and game logic
-        self.player_change_x = player_change_x
-        self.player_change_y = player_change_y
-        self.update_animation(delta_time)
+        self.update_animation(delta_time, player_change_x, player_change_y)
 
-        if self.player_state == WALK:
+        if self._player_state == WALK:
             self.update_movement()
 
         return self.player_texture
 
-    def update_animation(self, delta_time):
+    def update_animation(self, delta_time, player_change_x=0, player_change_y=0):
 
         # Figure out if we need to flip face left or right
-        if self.player_change_y < 0 and self.character_face_direction != DOWN_FACING:
+        if player_change_y < 0 and self.character_face_direction != DOWN_FACING:
             self.character_face_direction = DOWN_FACING
-        if self.player_change_y > 0 and self.character_face_direction != UP_FACING:
+        if player_change_y > 0 and self.character_face_direction != UP_FACING:
             self.character_face_direction = UP_FACING
-        if self.player_change_x < 0 and self.character_face_direction != LEFT_FACING:
+        if player_change_x < 0 and self.character_face_direction != LEFT_FACING:
             self.character_face_direction = LEFT_FACING
-        if self.player_change_x > 0 and self.character_face_direction != RIGHT_FACING:
+        if player_change_x > 0 and self.character_face_direction != RIGHT_FACING:
             self.character_face_direction = RIGHT_FACING
 
         """ Update the animation state of the player """
         self.animation_timer += delta_time
 
         if self.animation_timer > ANIMATION_SPEED:
-            current_animation: Animation = self.all_animations[self.player_state]
+            current_animation: Animation = self.all_animations[self._player_state]
             self.player_texture, is_animation_finished = current_animation.get_next_texture(self.character_face_direction)
 
             if is_animation_finished:
@@ -110,39 +107,25 @@ class Animations():
     def finish_action(self):
         """ Handle finishing an action and transition to the appropriate state """
         if self.move_direction != (0, 0):
-            self.player_state = WALK
+            self._player_state = WALK
             self.update_movement()
         else:
-            self.player_state = IDLE
+            self._player_state = IDLE
 
     def update_movement(self):
         """ Update the player's movement based on the keys pressed """
         self.action_finished = 1
 
         if self.move_direction[0] == 0 and self.move_direction[1] == 0:
-            self.player_state = IDLE
+            self._player_state = IDLE
 
     def on_key_press(self, key, modifiers):
         """ Called whenever a key is pressed. """
-
-        if self.player_state not in [SLASH, SPELLCAST, THRUST, SHOOT, DIE]:
-            self.player_state = WALK
-        if key == self.slash_key and self.player_state not in [SLASH, SPELLCAST, THRUST, SHOOT, DIE]:
-            self.player_state = SLASH
-            self.slash_texture_index = 0
-            self.action_finished = 0
-        elif key == self.cast_key and self.player_state not in [SLASH, SPELLCAST, THRUST, SHOOT, DIE]:
-            self.player_state = SPELLCAST
-            self.spellcast_texture_index = 0
-            self.action_finished = 0
-        elif key == self.thrust_key and self.player_state not in [SLASH, SPELLCAST, THRUST, SHOOT, DIE]:
-            self.player_state = THRUST
-            self.thrust_texture_index = 0
-            self.action_finished = 0
-        elif key == self.shoot_key and self.player_state not in [SLASH, SPELLCAST, THRUST, SHOOT, DIE]:
-            self.player_state = SHOOT
-            self.shoot_texture_index = 0
-            self.action_finished = 0
+        
+        if self._player_state  not in self.key_to_action_dict.values():
+            self._player_state = self.key_to_action_dict.get(key, WALK)
+            if self._player_state != WALK:
+                self.action_finished = 0
 
     def on_key_release(self, key, modifiers):
         """ Called whenever a key is released. """
@@ -152,5 +135,5 @@ class Animations():
     def check_movement_keys(self):
         """ Check if any movement keys are pressed and update the state """
         if self.move_direction != (0, 0):
-            if self.player_state == IDLE:
-                self.player_state = WALK
+            if self._player_state == IDLE:
+                self._player_state = WALK
